ee3d83da64b5bb2b2086beb7d826ce7bb6e5a161
Danish Siddiqui <danish9412@gmail.com>
2019-02-21 19:33:04 -0500

    getting all HTML data from google news and making a page context from bs

--- NewsEmotions.py
+++ NewsEmotions.py
@@ -6,7 +6,6 @@ Created on Thu Feb 21 15:58:08 2019
 @author: danishsiddiqui
 """
 import requests
-import urllib2
 from bs4 import BeautifulSoup
 
 def google_news_grabber(stockName):
@@ -15,11 +14,12 @@ def google_news_grabber(stockName):
     url = 'https://news.google.com/rss/search?q='+stockName+'&hl=en-US&gl=US&ceid=US:en'
     response = requests.get(url)
     page = response.content
-    print("page: ",page);
-    soup = BeautifulSoup(page, ‘html.parser’)
+    #print("page: ",page);
+    soup = BeautifulSoup(page, 'html.parser')
+    print("soup: ",soup.find_all('description'))
     textContent = []
-    for i in range(0, 50):
-        paragraphs = page_content.find_all("p")[i].text
+    for i in soup.find_all('description'):
+        paragraphs = soup.find_all('p')[i].text
         textContent.append(paragraphs)
     
   
